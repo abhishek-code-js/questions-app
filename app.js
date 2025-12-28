48211bd5598d024b75c6af73e8036be576ff13ec
Abhishek Shah <abhishekmac@Abhisheks-MacBook-Pro.local>
2025-12-28 15:19:35 +0530

    added logic to implement accordion -using selector inside an element method

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 //using selectors inside the element
 // traversing the dom
 
-const btns = document.querySelectorAll(".question-btn");
+// const btns = document.querySelectorAll(".question-btn");
 
-btns.forEach(function (btn) {
-  btn.addEventListener("click", (e) => {
-    const question = e.currentTarget.parentElement.parentElement;
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", (e) => {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//   });
+// });
+
+// using selectors inside the element
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((question) => {
+  //   console.log(question);
+  const btn = question.querySelector(".question-btn");
+  btn.addEventListener("click", () => {
+    questions.forEach((item) => {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
     question.classList.toggle("show-text");
   });
 });
